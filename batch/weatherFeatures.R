f4e8cfaa5b89efe44afd7d62ce5781ecdb7d86d1
Sam Borgeson <sborgeson@berkeley.edu>
2013-02-23 09:05:42 -0800

    full model runs working

--- batch/weatherFeatures.R
+++ batch/weatherFeatures.R
@@ -1,16 +1,26 @@
 weatherFeatures = function(w){ # r is an instance of WeatherClass
   wMeans = colMeans(w$rawData[,-1],na.rm=T)
-  features = c(w$zip,wMeans)
+  features = c(zip5=w$zip,wMeans)
   return(features)
 }
 
-zips = db.getZips()
-i = 0
-n = length(zips)
-wList = as.list(rep(NA,length(zips)))
-for(zip in zips) {
-  i = i+1
-  print(paste(zip,'(',i,'/',n,')'))
-  wList[[i]] = weatherFeatures(WeatherClass(zip,doMeans=F))
+getWeatherSummary = function(w) {
+  summaryFile = paste(getwd(),'/weatherSummary.RData',sep='')
+  if(file.exists(summaryFile)) {
+    load(summaryFile)
+  }
+  else {
+    zips = db.getZips()
+    i = 0
+    n = length(zips)
+    wList = as.list(rep(NA,length(zips)))
+    for(zip in zips) {
+      i = i+1
+      print(paste(zip,'(',i,'/',n,')'))
+      wList[[i]] = weatherFeatures(WeatherClass(zip,doMeans=F))
+    }
+    weatherSummary = do.call(rbind,wList)
+    save(weatherSummary,file=summaryFile)
+  }
+  return(weatherSummary)
 }
-weatherSummary = do.call(rbind,wList)
