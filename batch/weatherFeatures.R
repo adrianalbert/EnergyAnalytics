31184f1c1facd27e7a3dd807050192d9cb8340e9
Sam Borgeson <sborgeson@berkeley.edu>
2013-04-04 19:20:46 -0700

    weatherFeatures

---
+++ batch/weatherFeatures.R
@@ -0,0 +1,16 @@
+weatherFeatures = function(w){ # r is an instance of WeatherClass
+  wMeans = colMeans(w$rawData[,-1],na.rm=T)
+  features = c(w$zip,wMeans)
+  return(features)
+}
+
+zips = db.getZips()
+i = 0
+n = length(zips)
+wList = as.list(rep(NA,length(zips)))
+for(zip in zips) {
+  i = i+1
+  print(paste(zip,'(',i,'/',n,')'))
+  wList[[i]] = weatherFeatures(WeatherClass(zip))
+}
+weatherSummary = do.call(rbind,wList)
