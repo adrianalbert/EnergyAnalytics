4ef4f176cf9f57d17e50c58e65601a42164b7c73
adrian <adalbert@stanford.edu>
2014-03-01 14:51:23 -0800

    added more pecan st code

--- batch/pecan_download.r
+++ batch/pecan_download.r
@@ -45,7 +45,7 @@ parse_data = function(data, selection = NULL) {
   if (!is.null(selection)) data = subset(data, select = selection)  
   
   # only store columns that have actual data
-  col.na = which(sapply(data, function(x)all(is.na(x))))
+  col.na = which(sapply(data, function(x)all(is.na(x))) | sapply(data, function(x)all(x==0)))
   if (length(col.na)>0) data = data[,-col.na] else return(NULL)
   
   # if too little data return null
@@ -87,19 +87,19 @@ for (yr in years) {
 # ------------------------------------------
 # Save meta-data on users
 # ------------------------------------------
-for (yr in c('2012', '2013')) {
-  # get survey data
-  query    = paste("select * from \"PecanStreet_RawData\".participant_survey_", yr, sep = '')
-  cur_data = dbGetQuery(con, query)   
-  filepath = file.path(DUMP_PATH, paste('metadata/survey_', yr, '.csv', sep=''))
-  write.csv(cur_data, file = filepath)    
-
-  # get survey data description
-  query    = paste("select * from \"PecanStreet_RawData\".participant_survey_", yr, "_field_descriptions", sep = '')
-  cur_data = dbGetQuery(con, query)   
-  filepath = file.path(DUMP_PATH, paste('metadata/survey_desc_', yr, '.csv', sep=''))
-  write.csv(cur_data, file = filepath)    
-}
+# for (yr in c('2012', '2013')) {
+#   # get survey data
+#   query    = paste("select * from \"PecanStreet_RawData\".participant_survey_", yr, sep = '')
+#   cur_data = dbGetQuery(con, query)   
+#   filepath = file.path(DUMP_PATH, paste('metadata/survey_', yr, '.csv', sep=''))
+#   write.csv(cur_data, file = filepath)    
+# 
+#   # get survey data description
+#   query    = paste("select * from \"PecanStreet_RawData\".participant_survey_", yr, "_field_descriptions", sep = '')
+#   cur_data = dbGetQuery(con, query)   
+#   filepath = file.path(DUMP_PATH, paste('metadata/survey_desc_', yr, '.csv', sep=''))
+#   write.csv(cur_data, file = filepath)    
+# }
 
 # ------------------------------------------
 # Parse and dump time series data
@@ -111,15 +111,19 @@ for (yr in names(uids)) {
   yr       = as.numeric(yr)
   res      = lapply(1:length(cur_uids), function(i) {       
     uid = cur_uids[i]
-    cat(paste('Fetching user ', i, '/', length(cur_uids), ' (ID=', uid, ')\n', sep = ''))
+    cat(paste('Fetching user ', i, '/', length(cur_uids), ' (ID=', uid, ')', sep = ''))
     
-    # get current data    
-    query    = paste("select * from \"PecanStreet_RawData\".egauge_minutes_", yr, " where dataid = ", uid, sep = '')
+    # get current data  
+    fields   = paste(c(select.keep, select.AC, select.HV, select.light), collapse = ',')
+    query    = paste("select ", fields, " from \"PecanStreet_RawData\".egauge_minutes_", yr, " where dataid = ", uid, sep = '')
     cur_data = dbGetQuery(con, query)   
         
     # parse data 
     ok_data = parse_data(cur_data, selection = c(select.keep, select.AC, select.HV, select.light))
-    if is.null(ok_data) return(-1)
+    if (is.null(ok_data)) {
+      cat('...not enough data!\n')
+      return(-1)
+    }
     
     # save to CSV file; if file exists, append
     filename = paste('usage/', uid, '.csv', sep = '')
@@ -129,6 +133,7 @@ for (yr in names(uids)) {
     } else {
       write.csv(ok_data, file = filepath)
     }    
+    cat('...done\n')
   
     # return columns available for each user
     return(setdiff(names(data), select.keep))
