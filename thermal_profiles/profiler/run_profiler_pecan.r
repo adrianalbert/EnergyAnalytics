04a0a9044fd61dea02cd6b58c537c9656d1a0fa4
adrian <adalbert@stanford.edu>
2014-05-02 03:07:41 -0700

    some re-organizing

--- thermal_profiles/profiler/run_profiler_pecan.r
+++ thermal_profiles/profiler/run_profiler_pecan.r
@@ -16,8 +16,8 @@ library('segmented')
 setwd('~/EnergyAnalytics/thermal_profiles/profiler/')
 source('stateProcessorWrapper.r')
 source('stateVisualizerWrapper.r')
-source('../../utils/aggregate_data.r')
 
+DATA_PATH  = '~/energy-data/pecan_street/usage-processed/2012/'
 PLOTS_PATH = '~/Dropbox/OccupancyStates/plots/pecan-street'
 dir.create(PLOTS_PATH)
 
@@ -27,100 +27,107 @@ weather.15mins = read.csv('~/energy-data/pecan_street/weather/weather_15mins.csv
 weather.hourly$date = as.POSIXct(as.character(weather.hourly$date))
 weather.15mins$date = as.POSIXct(as.character(weather.15mins$date))
 
+# load baby names
+baby_names  = read.csv('~/Dropbox/OccupancyStates/data/baby-names.csv')
+
 # __________________________________________________
 # Select appliances of interest
 
 HVAC   = c('AIR1','AIR2','FURNACE1','FURNACE2')
 HV     = c('FURNACE1','FURNACE2')
 AC     = c('AIR1','AIR2')
-fridge = c('REFRIGERATOR1')
-dhw    = c('WATERHEATER1')
-aux    = c('SPRINKLER1')
-lights = c('LIGHTING_PLUGS1','LIGHTING_PLUGS2','LIGHTING_PLUGS3','LIGHTING_PLUGS4')
-user   = c('DININGROOM1','DISHWASHER1','DRYG1','KITCHEN1','KITCHEN2','KSAC1','KSAC2','SECURITY1',           
-           'LIVINGROOM1','MASTERBATH1','MICROWAVE1','OVEN1','RANGE1','WASHER1','WASHINGMACHINE1',
-           'BATHROOM1','BEDROOM1','BEDROOM2','DISPOSAL1','DRYE1','FAMROOM1','BATH1','DRYER2','GARAGE','GARAGE1',
-           'GENLIGHT1','SMALLAPPLIANCE1','SMALLAPPLIANCE2','SMALLAPPLIANCE3','COOKTOP1',
-           'BACKYARD1','OFFICE1','TVROOM1','THEATER1','MASTERBED1')
+lights = c('LIGHTS_PLUGS1','LIGHTS_PLUGS2','LIGHTS_PLUGS3','LIGHTS_PLUGS4')
 total   = c('USE')
-solar   = c('GEN')
-ev      = c('CAR1')
-net     = c('Grid')
 
 # __________________________________________________
-# Plot usage by minute & by hour
+# Load up user data
 
-ppw = pecanCombinedData
-if (1 == 1) {
-for(homeName in names(ppw)){
-  print(homeName)
-  homeData = ppw[[homeName]]
-  #print(names(homeData))
-  print(dim(homeData))
-	homeName = trim(homeName)
+# list all data files for 2013
+files    = list.files(path=DATA_PATH, full.names = T)
+files_01 = files[grep('minute',files)]
+files_15 = files[grep('15mins',files)]
+files_60 = files[grep('hourly', files)]
 
+# __________________________________________________
+# Plot usage by minute & by hour for each user
+
+# aggregate data from columns defined by labels
+usage = function(data,labels) {
+  sub = match(labels,names(data))
+  sub = sub[!is.na(sub)]
+  return(apply(as.matrix(data[,sub]),1,sum))
+}
+
+# plot ground truth components
+plot_user = function(homeData, main = 'minute') {
+  
+  names(homeData) = toupper(names(homeData))
+  
+  # aggregate components
+  AC_kwh        = usage(homeData,AC)
+  HV_kwh        = usage(homeData,HV)
+  total_kwh     = usage(homeData,total)
+  occupancy_kwh = total_kwh - AC_kwh - HV_kwh
+  
+  maxUsage = max(total_kwh)
+  plot(homeData$DATE,total_kwh,type='l',col=cols[1],main=main,ylab='kW',ylim=c(0,1.1*maxUsage),xaxt='n')
+  points(homeData$DATE,AC_kwh,type='l',col=cols[3])
+  points(homeData$DATE,HV_kwh,type='l',col=cols[2])
+  points(homeData$DATE,occupancy_kwh,type='l',col=cols[4])
+  if ('TEMPERATUREF' %in% names(homeData))
+    points(homeData$DATE,homeData$TEMPERATUREF/100*maxUsage,type='l',col=cols[5])
+  d = homeData$DATE
+  dts = seq(d[1],d[length(d)],by=3600*24) # one per day from one per minute
+  axis(1, dts, format(dts, "%a, %m/%d"), cex.axis=1)
+  grid(nx=NA,ny=NULL)
+  abline(v=dts,col="black",lty=3)
+  
+}
+
+all_data = list()
+for(i in 1:length(files_01)){
+
+  # some gymnastics to get user ID
+  userID = strsplit(rev(strsplit(files_01[i], '/')[[1]])[1], '_')[[1]][1]
+  
+  # read in data at different resolutions...
+  cat(paste(files_01[i], files_15[i], files_60[i], '...\n'))
+  data_01 = read.csv(files_01[i]); data_01$localminute = as.POSIXct(data_01$localminute); names(data_01)[1] = 'date';
+  data_15 = read.csv(files_15[i]); data_15$date = as.POSIXct(data_15$date);
+  data_60 = read.csv(files_60[i]); data_60$date = as.POSIXct(data_60$date);
+  
+  # add in weather (temperature) data
+  data_15 = merge(data_15, subset(weather.15mins, select = c('date', 'TemperatureF')), by = 'date')
+  data_60 = merge(data_60, subset(weather.hourly, select = c('date', 'TemperatureF')), by = 'date')  
+  
   # define plot parameters
   op <- par(no.readonly = TRUE)
-  cols = c('black','#FE2E2E','#0040FF','#ff9999', '#bbbbbb')
   m <- matrix(c(1,2,3,4),nrow=4,ncol=1,byrow=T)
   layout(mat = m,heights = c(0.3,0.3,0.3,0.1))
   par(oma=c(2,2,2,0),mar=c(2,4,2,1)) # Room for the title
   
+  cols = c('black','#FE2E2E','#0040FF', '#088A08', '#424242')
   # print minute-by-minute data
-  maxUsage = max(usage(homeData,total))
-  plot(homeData$date,usage(homeData,total),type='l',col=cols[1],main='minute',ylab='kW',ylim=c(0,1.1*maxUsage),xaxt='n')
-  points(homeData$date,usage(homeData,AC),type='l',col=cols[3])
-  points(homeData$date,usage(homeData,HV),type='l',col=cols[2])
-  points(homeData$date,usage(homeData,user),type='l',col=cols[4])
-  points(homeData$date,homeData$TemperatureF/100*maxUsage,type='l',col=cols[5])
-  d = homeData$date
-  dts = seq(d[1],d[length(d)],by=3600*24) # one per day from one per minute
-  axis(1, dts, format(dts, "%a, %m/%d"), cex.axis=1)
-  grid(nx=NA,ny=NULL)
-  abline(v=dts,col="black",lty=3)
-  
-  # aggregate data to 10 minutes  
-  min10Data = to10min(homeData)
-  maxUsage = max(usage(min10Data,total))
-  plot(min10Data$date,usage(min10Data,total),type='l',col=cols[1],main='10-minute',ylab='kW',ylim=c(0,1.1*maxUsage),xaxt='n')
-  points(min10Data$date,usage(min10Data,AC),type='l',col=cols[3])
-  points(min10Data$date,usage(min10Data,HV),type='l',col=cols[2])
-  points(min10Data$date,usage(min10Data,user),type='l',col=cols[4])
-  points(min10Data$date,min10Data$TemperatureF/100*maxUsage,type='l',col=cols[5])
-  d = min10Data$date
-  dts = seq(d[1],d[length(d)],by=3600*24) # one per day from one per minute
-  axis(1, dts, format(dts, "%a, %m/%d"), cex.axis=1)
-  grid(nx=NA,ny=NULL)
-  abline(v=dts,col="black",lty=3)
-  
+  print(plot_user(subset(data_01, date > as.POSIXct('2012-08-01') & date < as.POSIXct('2012-08-14')), main = 'minute'))
+  # print 15 min data
+  print(plot_user(subset(data_15, date > as.POSIXct('2012-08-01') & date < as.POSIXct('2012-08-14')), main = '15 minute'))
   # print hourly data
-  hrData = toHourly(homeData)
-  maxUsage = max(usage(hrData,total))
-  plot(hrData$date,usage(hrData,total),type='l',col=cols[1],main='hourly',ylim=c(0,1.1*maxUsage),ylab='kW',xaxt='n')
-  points(hrData$date,usage(hrData,HV),type='l',col=cols[2])
-  points(hrData$date,usage(hrData,AC),type='l',col=cols[3])
-  points(hrData$date,usage(hrData,user),type='l',col=cols[4])
-  points(hrData$date,hrData$TemperatureF/100*maxUsage,type='l',col=cols[5])
-  d = as.POSIXct(hrData$date)
-  dts = seq(d[1],d[length(d)],by=3600*24) # one per day from one per hour
-  axis(1, dts, format(dts, "%a, %m/%d"), cex.axis=1)
-  grid(nx=NA,ny=NULL)
-  abline(v=dts,col="black",lty=3)
-  mtext(paste('Pecan Street example','kW demand'), line=0, font=2, cex=1.2, outer=TRUE)
+  print(plot_user(subset(data_60, date > as.POSIXct('2012-08-01') & date < as.POSIXct('2012-08-14')), main = 'hourly'))
+  
+  mtext(paste('Pecan Street Experiment User', userID), line=0, font=2, cex=1.2, outer=TRUE)
   par(mar=c(1,4,1,1))
   plot.new()
   legend("center", lty=1,cex=1,lwd=2,
-         legend=c('total','HV', 'AC', 'user','temperature'), 
+         legend=c('total','HV', 'AC', 'occupant', 'temperature (F)'), 
          col=cols,horiz=T)
-  # par(op)
-  #break
-	dir.create(file.path(PLOTS_PATH, homeName))
-  dev.copy2pdf(file=paste(paste(PLOTS_PATH, homeName, sep = '/'), paste(homeName,'.pdf',sep=''), sep='/'),width=10,height=6)
-}
-}
 
-#plot(homeData$date,usage(homeData,'THEATER1'),type='l')
-#points(homeData$date,usage(homeData,fridge),type='l',col='green')
+  # save plot to file
+  dir.create(file.path(PLOTS_PATH, userID))
+  dev.copy2pdf(file=paste(paste(PLOTS_PATH, userID, sep = '/'), paste(userID,'.pdf',sep=''), sep='/'),width=10,height=6)
+  
+  # store data for later processsing
+  all_data[[userID]] = list(min_15 = data_15, min_60 = data_60)
+}
 
 # __________________________________________________
 # Apply Thermal States model to Pecan data
