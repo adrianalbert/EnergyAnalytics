bd9fa516532961486d0a65be79657046c88da3be
adrian <adalbert@stanford.edu>
2014-05-27 08:51:37 -0700

    cleanup

--- batch/pecan/pecan_validate.r
+++ batch/pecan/pecan_validate.r
@@ -13,13 +13,15 @@ options(error = recover)
 # Initializations...
 
 library('ggplot2')
+source('../../thermal_profiles/validator/')
 
-DATA_PATH = '~/energy-data/pecan_street/models/'
+
+DATA_PATH = '~/energy-data/pecan_street/usage-select/'
 DUMP_PATH = '~/energy-data/pecan_street/models/'
 PLOT_PATH = '~/Dropbox/OccupancyStates/plots/pecan-street/validation/'
 
 # list all data files by year/uid
-files.decode = list.files(path=DATA_PATH, pattern = '*_decoded*', full.names = T, recursive = T)
+files.decode = list.files(path=DUMP_PATH, pattern = '*_decoded*', full.names = T, recursive = T)
 files.interp = list.files(path=DUMP_PATH, pattern = '*_interpreted*', full.names = T, recursive = T)
 user_info  = lapply(files.decode, function(x) {
   tmp = strsplit(x, '/')[[1]]
@@ -37,9 +39,29 @@ user_info$res= as.character(user_info$res)
 # __________________________________________________
 # Function to load data and compute metrics
 
-file.d = files.decode[1]
-file.i = files.interp[1]
+f = 1
+file.d = files.decode[f]; load(file.d); decode_info = data
+file.i = files.interp[f]; load(file.i); interp_info = data
+file.r = file.path(DATA_PATH, user_info[f, 'res'], user_info[f, 'yr'], paste(user_info[f, 'uid'], '.csv', sep=''))
+data   = read.csv(file.r)
 
-compute_metrics = function(data)
+# prepare data for analysis
+prepare_data = function(decode_info, interp_info, data) {
+  
+  # access data
+  nTrain = length(decode_info$state)
+  nStates= decode_info$nStates
+  df     = data[1:nTrain, ]; df$state = decode_info$states; 
+  df$TemperatureD = df$TemperatureF - 65
+  resp   = decode_info$response   
+  tran   = decode_info$transition
+  tran   = lapply(unique(tran$From), function(s) {d = subset(tran, From ==s); d$From = d$To = NULL; rownames(d) = 1:nrow(d); return(d)})
+  
+  return(list(resp = resp, tran = tran, data = df))
+}
+  
+# function to run analysis at different levels of data resolution
 
 
+res = prepare_data(decode_info, interp_info, data)
+df = res$data
