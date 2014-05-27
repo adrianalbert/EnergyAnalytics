51de4c8215f0b1e3eface8982b18f31804e26b15
adrian <adalbert@stanford.edu>
2014-05-27 08:52:22 -0700

    added validation code

---
+++ thermal_profiles/validator/plot_metrics.r
@@ -0,0 +1,8 @@
+# plot_metrics.r
+#
+# Plot metrics to validate individual model performance. 
+#
+# Adrian Albert
+# Last modified: June 2014.
+# ---------------------------------------------------------
+
