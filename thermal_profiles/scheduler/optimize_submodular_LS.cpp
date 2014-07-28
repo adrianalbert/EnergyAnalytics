2f90fa2dadf5aee80b6c5342002d84f1a73f16a6
Adrian Albert <adrian.t.albert@gmail.com>
2014-07-28 11:58:24 -0700

    debugging submodular optimization C++ implementatiion

--- thermal_profiles/scheduler/optimize_submodular_LS.cpp
+++ thermal_profiles/scheduler/optimize_submodular_LS.cpp
@@ -57,16 +57,113 @@ Rcpp::NumericVector compute_objective_quad(Rcpp::NumericMatrix& Abar_,
 */  
 
 // [[Rcpp::export]]
-Rcpp::List optimize_submodular_LS(Rcpp::List& Omega, Rcpp::List& U_list, Rcpp::List& params)  {
+Rcpp::List optimize_submodular_LS(Rcpp::List Omega, Rcpp::List U_list, Rcpp::List params)  {
+
+
+  cout<<"Initializing objects...\n";
+
+  // define objects and access data
   
-  // define objects
-  arma::colvec q = as<arma::colvec>(params['q']);
-  arma::colvec g = as<arma::colvec>(params['g']);
-  arma::mat Abar, U;
-  arma::mat W    = zeros<arma::mat>(Abar.n_rows, Abar.n_cols);
   int N = Omega.size(); 
-  for (int i=0; i<N; i++) {
-    arma::mat Wt = W_[i];
+  arma::colvec q = as<arma::colvec>(params["q"]);
+  arma::colvec g = as<arma::colvec>(params["g"]);
+  double eps     = params["eps"];
+  int tau = g.size();
     
-  // 
+  // format arrays
+  arma::mat W    = zeros<arma::mat>(N, tau), Wi;
+  arma::mat Abar = zeros<arma::mat>(N, tau);
+  Rcpp::List Oi; arma::vec Ai;
+  for (int i=0; i<N; i++) {
+    Oi = Omega[i]; Wi = as<arma::mat>(Oi["w"]); Ai = as<arma::vec>(Oi["a"]);
+    arma::vec Wid = Wi.diag();
+    W.row(i) = Wid.t();
+    Abar.row(i) = Ai.t();
+  }
+
+  arma::mat UL    = U_list["UL"];   // list of all schedules
+  arma::colvec UA = U_list["UA"];  // indicates which user each schedule belongs to
+  UA = UA - ones<arma::colvec>(UA.size());
+  int Nu = UL.n_rows, e;
+
+  // initialize sets as 0/1 arrays
+  cout<<"Initializing sets..."<<'\n';  
+  arma::uvec   A = zeros<arma::uvec>(N);
+  arma::uvec   U = -ones<arma::uvec>(N);
+  arma::colvec d = zeros<arma::colvec>(N);
+  for (int v = 0; v<Nu; v++) {
+    e = UL[v];
+    arma::uvec e_vec, v_vec; e_vec<<e; v_vec<<v;
+    arma::mat Acur = Abar.rows(e_vec), Wcur = W.rows(e_vec), Ucur = UL.rows(v_vec);
+    d[v] = compute_objective(Acur, Wcur, Ucur, g, q);
+  }
+  arma::vec tmp = d.elem(find(d == max(d))); int m = tmp[0];
+  A[UA[m]] = 1;
+  U[UA[m]] = m;
+  
+//  // main loop
+//  arma::uvec A1, U1;
+//  int iter = 0, n;
+//  bool terminate;
+//  do {
+//    
+//    iter = iter + 1;
+//    terminate = 1;
+//    cout<<"Step forward: Iteration "<<iter<<'\n';
+//    
+//    uvec tmp = find(A > 0); n = tmp.size();
+//    arma::uvec e_vec = find(A > 0); 
+//    arma::uvec v_vec = U.elem(find(U > 0));
+//    arma::mat Acur = Abar.rows(e_vec), 
+//              Wcur = W.rows(e_vec), 
+//              Ucur = UL.rows(v_vec);
+//
+//    // find appropriate element
+//    bool backstep = 1;
+//    for (int v = 0; v < Nu; v++) 
+//      if (A[UA[v]] == 0) {      // if element v is not in A yet
+//        e = UA[v]; 
+//        A1 = A; A1[e] = 1;
+//        U1 = U; U1[e] = v;
+//        arma::uvec e_vec1= A1.elem(find(A1 > 0)), v_vec1 = U1.elem(find(U1 > 0));
+//        arma::mat Acur1= Abar.rows(e_vec1), Wcur1 = W.rows(e_vec1), Ucur1 = UL.rows(v_vec1);
+//        double obj1 = compute_objective(Acur1, Wcur1, Ucur1, g, q);
+//        double obj  = compute_objective(Acur, Wcur, Ucur, g, q);
+//        if (obj1 > (1 + eps/(n*n))*obj) {
+//          A[UA[v]] = 1;
+//          U[UA[v]] = v;
+//          backstep = 0;
+//          terminate = 0;
+//          break;
+//        }        
+//      }    
+//    
+//    // take step backward
+//    if (!backstep) continue;
+//    
+//    cout<<"Step backward: Iteration "<<iter<<'\n';
+//        
+//    for (int e = 0; e < N; e++) 
+//      if (A[e] == 1) {      // if element v is in A
+//        A1 = A; A1[e] = 0;
+//        U1 = U; U1[e] = -1;
+//        arma::uvec e_vec1= A1.elem(find(A1 > 0)), v_vec1 = U1.elem(find(U1 > 0));
+//        arma::mat Acur1= Abar.rows(e_vec1), Wcur1 = W.rows(e_vec1), Ucur1 = UL.rows(v_vec1);
+//        double obj1 = compute_objective(Acur1, Wcur1, Ucur1, g, q);
+//        double obj  = compute_objective(Acur, Wcur, Ucur, g, q);
+//        if (obj1 > (1 + eps/(n*n))*obj) {
+//          A[e] = 0;
+//          U[e] = -1;
+//          terminate = 0;
+//          break;
+//        }        
+//      };   
+//      
+//    
+//    
+//  } while (!terminate && n>0);
+  
+  // return value
+  return(Rcpp::List::create(Rcpp::Named("A")=0 , Rcpp::Named("U")=0));
+
 }
--- thermal_profiles/scheduler/optimize_submodular_LS.r
+++ thermal_profiles/scheduler/optimize_submodular_LS.r
@@ -3,31 +3,48 @@ library('RcppArmadillo')
 
 rm(list = ls())
 
+setwd('~/EnergyAnalytics/thermal_profiles/scheduler/')
+
 # compile functions
 sourceCpp('optimize_submodular_LS.cpp')
 
-# generate fake data
-A = matrix(runif(N*24), ncol = 24)
-U = matrix(runif(N*24), ncol = 24)
-W = lapply(1:N, function(i) matrix(runif(24*24), ncol = 24))
-res = compute_objective_quad(A, W, U, g, q)
-  
-# g = runif(24)
-# q = runif(24)
-# N = 100;   
-# zeta = sample(2:6, N, replace = T)
-# usr_names = paste('Name', 1:N)
-# Omega = lapply(usr_names, function(s) runif(24))
-# names(Omega) = usr_names
-# U_list= lapply(zeta, function(z) {
-#   eta   = sample(1:23, z, replace = T)
-#   gamma = sample(1:5, z, replace = T)
-#   U     = lapply(1:z, function(s) {
-#     u = rep(0, 24)
-#     u[eta[s]:min(eta[s] + gamma[s], 24)] = 1
-#     return(u)
-#   })
-#   return(U)
-# })
-# names(U_list) = usr_names
+# # ___________________________________________
+# # Test objective function 
 # 
+# # generate fake data
+# N = 100
+# A = matrix(runif(N*24), ncol = 24)
+# U = matrix(runif(N*24), ncol = 24)
+# W = lapply(1:N, function(i) matrix(runif(24*24), ncol = 24))
+# 
+# # test objective
+# res = compute_objective_quad(A, W, U, g, q)
+# 
+# # ___________________________
+# Test optimization algorithm
+
+# generate fake data
+g = runif(24)
+q = runif(24)
+N = 100;   
+zeta = sample(2:6, N, replace = T)
+usr_names = paste('Name', 1:N)
+Omega = lapply(usr_names, function(s) list(a = runif(24), w = matrix(runif(24*24), ncol=24)))
+names(Omega) = usr_names
+U_list= lapply(zeta, function(z) {
+  eta   = sample(1:23, z, replace = T)
+  gamma = sample(1:5, z, replace = T)
+  U     = lapply(1:z, function(s) {
+    u = rep(0, 24)
+    u[eta[s]:min(eta[s] + gamma[s], 24)] = 1
+    return(u)
+  })
+  return(U)
+})
+names(U_list) = usr_names
+UL = do.call('rbind', sapply(U_list, function(l) do.call('rbind', l)))
+UA = rep(1:N, sapply(U_list, length))
+
+# test optimization
+params = list(g = g, q = q, eps = 0.01)
+res = optimize_submodular_LS(Omega, list(UL = UL, UA = UA), params)
